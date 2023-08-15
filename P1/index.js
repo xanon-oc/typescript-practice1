4f1128dcfa10580ef233a84403b1eaab4431aeeb
Niloy Roy <niloyroy184@gmail.com>
2023-08-15 11:18:38 +0600

    pratice

---
+++ P1/index.js
@@ -0,0 +1,7 @@
+var myName = "Niloy Roy";
+var herName = "Janina Se ke !";
+console.log(myName + " " + herName);
+var addNumbers = function (num1, num2) {
+    console.log(num1 + num2);
+};
+addNumbers(10, 40);
---
+++ P1/index.ts
@@ -0,0 +1,9 @@
+let myName = "Niloy Roy";
+let herName = "Janina Se ke !";
+console.log(myName + " " + herName);
+
+const addNumbers = (num1: number, num2: number) => {
+  console.log(num1 + num2);
+};
+
+addNumbers(10, 40);
